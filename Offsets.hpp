abbceda4ac47b35ba0832573b86328c6ce34bcab
IsabellaDev <alexhusum7@gmail.com>
2024-07-08 22:10:40 +0200

    Create Offsets.hpp

---
+++ Offsets.hpp
@@ -0,0 +1,48 @@
+Offsets below:
+
+Updated as of 08/07/2024
+
+
+
+  
+init_ray_cast_struct :0xF36100
+
+FN_Raycast Func is Called at: 0xB44D15
+FN_ray_cast Func: 0xF3A900
+
+Ray_Force Func is Called at: 0xFCC144
+Ray_Force Func: 0x17A4C95
+
+Get_g_manager_from_component Func is Called at: 0x7F414D
+Get g_mamger Func: 0x76A870
+
+Get_hit_bone Func is Called at: 0xE10E47
+Get_hit_bone Func: 0x117F1E0
+
+Render Outline (VEH HOOK ADDR):0xEC0080
+skill_struct_check:0x23D5030
+fnskillstruct:0x23EEE00
+Camera_Manager(Unencrypted VM can be found here):0x254F76D
+Sensitive Ptr:0x205C
+
+
+Decrypt Visibility Func is Called at: 0x85B64C
+Decrypt Visbility Func: 0x7A6380
+Need to read for vis:0x37CA340
+Vis FN:0x7AFA72
+Visbility Xor Key 1:0x6847903E1BDE5A37
+
+Decrypt Outline Func is Called at: 0x85E0E9
+Decrypt Outline Func: 0x7AFA60
+Need to read for outline:0x37CA340
+Outline FN:0x7AFA72
+OutLine Xor Key 1:0x6847903E1BDE5A37
+
+VMXorBase: 0x372C110
+VM XOR KEY:0x1603E7436E47D0CA
+Entity Admin: 0x3717600
+UnEncrypted VM: 0x3DEB808
+FOV changer: 0x388F7A8
+User CMD: 0xFFFF800841490000
+Sombra GLOW: 0xFFFF800841490000
+Get component Located: 0xFFFF800841490000
